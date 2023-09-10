d879ac4aa173fde2eeeda258821b9aa5375dda94
Sumanth Sura <126795010+sumanthsura@users.noreply.github.com>
2023-09-10 14:25:51 -0400

    replaced entire file

--- Lab2.rkt
+++ Lab2.rkt
@@ -14,7 +14,6 @@
 (define DISPLAY-X 150) ;; x coordinate of world's status
 (define DISPLAY-Y 100) ;; y coordinate of world's status
 
-;test
 
 (define WAKE-COW 0)
 (define SLEEPING-COW 1) 
@@ -57,25 +56,59 @@
 ;; Yes, I really followed HtDF for the helper functions.  You should too.
 ;; Debugging is so much easier when you can trust the building blocks work properly
 
-(define-struct CowWorld (text ListOfCow TrafficLights))
+(define-struct CowWorld (text cows TrafficLights hay time))
 
-; interp. Represents a display containing text describing the state of the world (text), a list of cows displayed as images, and traffic lights monitoring the cows' movement
-; CowWorld is (make-CowWorld Image ListOfCow TrafficLights)
-; !!
+; Data definition for CowWorld
+; interp. Represents a display containing text describing the state of the world, a list of cows displayed as images, and traffic lights monitoring the cows' movement
+; text: the status board containing the number of awake cows, sleeping cows, bales of hay, whether cows are stampeding, and time elapse. Image type
+; cows: ListOfCow type representing all the cows currently on the screen
+; TrafficLights: Image representing the status of traffic. Will be three circles (red, yellow, green in that order) on top of one another. At any given point in time, exactly one bulb will be lit. The lit bulb represents the state of the traffic. Red means awake cows stop when they get to the light, yellow means awake cows to the left of the traffic light go half speed, green means awake cows move normally.
+; hay: Natural representing the number of hay bales in the CowWorld
+; time: Integer type representing the time elapsed in the CowWorld in ticks
+; CowWorld is (make-CowWorld Image ListOfCow Image)
+
+; Template function for CowWorld
+(define (fn-for-CowWorld CowWorld)
+  (...
+   (CowWorld-text CowWorld)
+   (fn-for-loc (CowWorld-cows CowWorld))
+   (CowWorld-TrafficLights CowWorld)
+   (CowWorld-hay CowWorld)
+   (CowWorld-time CowWorld)))
+                
+
+(define-struct Cow (state x y speed))
 
 
-;!!! define ListOfCow
-; ListOfCow is one of:
+; Data definition for Cow
+; interp. A cow with a state, x- and y- positions and a speed.
+; state: A Natural type from 0 to 2 inclusive. 0 represents an awake cow, 1 represents a sleeping cow, 2 represents a stampeding cow
+; x: Natural representing the x-coordinate of the cow
+; y: Natural representing the y-coordinate of the cow 
+; speed is a Natural representing the speed of the cow in pixels per tick
+
+; Template function for Cow
+(define (fn-for-Cow Cow)
+  (...
+   (Cow-state Cow)
+   (Cow-x Cow)
+   (Cow-y Cow)
+   (Cow-speed Cow)))
+
+; Data definition for ListOfCow
+ ; ListOfCow is one of:
 ; - empty
 ; - (cons Cow ListOfCow)
 
-(define-struct Cow (state x y speed))
-; interp. A cow with a state (0, 1, 2) , x- and y- positions and a speed.
-; State represents one of the three Images, awake, asleep, and stampeding
-; x- and y- positions are Natural types
-; speed is a Natural 
-
-(define (main CowWorld)
+; Template function for ListOfCow
+(define (fn-for-loc loc)
+  (cond [(empty? loc) ...]
+        [else
+         (...
+          (fn-for-Cow (first loc))
+          (fn-for-loc (rest loc)))]))
+                   
+ (define (main CowWorld)
   (big-bang CowWorld
     (on-tick change)
     (to-draw render)
@@ -89,11 +122,6 @@
 ; changes the world state
 (define (change CowWorld) (make-CowWorld "a" "b" "c"))
 
-#;(define (change CowWorld)
-  (make-CowWorld ... (+ CowWorld-time 1) ...))
-
-
-
 ; CowWorld -> Image
 ;  !!!
 ; draws the world state
@@ -101,70 +129,23 @@
 (define (render CowWorld) MTS)
 
 #;(define (render CowWorld)
-  (StopLight CowWorld)
-  place (StatBoard CowWorld) x y MTS
-  (Cow CowWorld))
+
+    (StopLight CowWorld)
+    place (StatBoard CowWorld) x y MTS
+    (Cow CowWorld))
 
 ;;CowWorld -> Image
 ;;draws the StatBoard of current world state
-;;!!!
 
-(define (StatBoard CowWorld ListOfCow)
-  (above (text (append (NumSleeping ListOfCow) " of sleeeping cows" 24 "pink"))
-         (text (append (NumAwake ListOfCow) " of awake cows" 24 "pink"))
-         (text (append (CowWorld-hay CowWorld) " of hay bales" 24 "pink"))
-         (text (append (Stampeding? ListOfCow) " Stampeding?" 24 "pink"))
-         (text (append (ticks->seconds (CowWorld-time CowWorld)) " time elasped" 24 "pink"))))
-
-;;!!!
-;;ListOfCow -> Natural
-;;produces number of sleeping cows (1) in consumed CowWorld state
-
-(check-expect (NumSleeping empty) 0)
-(check-expect (NumSleeping               
-(check-expect (NumSleeping
-               
-;look up differences in equal funtions 
-
-;(define (NumSleeping LOC) 0)
-
-(define (NumSleeping LOC)
-  (cond [(empty? LOC) 0]
-        [else (if (= (Cow-state (first LOC)) 1)
-                  (+ 1 (NumSleeping (rest LOC)))
-                  (NumSleeping (rest LOC)))]))
-
-;;!!!
-;;ListOfCow -> Natural
-;;produces number of awake cows (0) in consumed CowWorld state
-
-(check-expect (NumAwake empty) 0)
-(check-expect (NumAwake
-(check-expect (NumAwake
-
-;(define (NumAwake LOC) 0)
-
-(define (NumAwake LOC)
-  (cond [(empty? LOC) 0]
-        [else (if (= (Cow-state (first LOC)) 0)
-                  (+ 1 (NumAwake (rest LOC)))
-                  (NumAwake (rest LOC)))]))
+(define (StatBoard CowWorld)
+  (above (text (append (fn-for-nsc CowWorld) " of sleeeping cows" 24 "pink"))
+         (text (append (fn-for-nac CowWorld) " of awake cows" 24 "pink"))
+         (text (append (fn-for-nhb CowWorld) " of hay bales" 24 "pink"))
+         (text (append (fn-for-yns CowWorld) " Stampeding?" 24 "pink"))
+         (text (append (fn-for-time CowWorld) " time elasped" 24 "pink"))))
 
 
-;;!!!
-;;ListOfCow -> Boolean
-;;produces true if concumed CowWorld states has stampeding cows (2)
-
-(check-expect (Stampeding empty) false)
-(check-expect (Stampeding
-(check-expect (Stampeding
-               
-;(define (Stampeding? LOC) false)
-
-(define (Stampeding? LOC)
-  (cond [(empty? LOC) false]
-        [(= (Cow-state (first LOC)) 2) true]
-        [else (Stampeding? (rest LOC))]))
+                             
 
 ;StopLight = generates light
 
@@ -183,4 +164,3 @@
         [else CowWorld]
 
         )
-  
